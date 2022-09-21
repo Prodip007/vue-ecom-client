6c7f7c4c983cc03bc6be307e0e932ee791d8edf8
Prodip <p.k.sorkar007@gmail.com>
2022-09-21 17:12:32 +0600

    Problem with add to cart

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,61 +10,60 @@
       <div class="column is-12">
         <h2 class="is-size-2 has-text-centered">Latest products</h2>
       </div>
-      <div class="column is-3" v-for="product in latestProducts" :key="product.id">
-        <div class="box">
-          <figure class="image md-4">
-            <img :src="product.get_thumbnail" alt="">
-          </figure>
-          <h3 class="is-size-4">{{ product.name }}</h3>
-          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
-          View details
+      <div class="columns">
+        <div class="column is-3" v-for="product in latestProducts" :key="product.id">
+          <div class="box">
+            <figure class="image md-4">
+              <img :src="product.get_thumbnail" alt="" />
+            </figure>
+            <h3 class="is-size-4">{{ product.name }}</h3>
+            <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+            <router-link :to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
+          </div>
         </div>
-
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   name: "Home",
 
   data() {
     return {
-      latestProducts: []
-    }
+      latestProducts: [],
+    };
   },
 
   components: {},
 
   mounted() {
-    this.getLatestProducts()
+    this.getLatestProducts();
   },
   methods: {
     getLatestProducts() {
       axios
-        .get('/api/v1/latest-products/')
-        .then(response => {
-          this.latestProducts = response.data
+        .get("/api/v1/latest-products/")
+        .then((response) => {
+          this.latestProducts = response.data;
         })
-        .catch(error => {
+        .catch((error) => {
           console.log(error);
-        })
-    }
-  }
-
+        });
+    },
+  },
 };
 </script>
 
 <style>
-  .image {
-    margin-top: -1.25rem;
-    margin-left: -1.25rem;
-    margin-right: -1.25rem;
-  }
+.image {
+  margin-top: -1.25rem;
+  margin-left: -1.25rem;
+  margin-right: -1.25rem;
+}
 </style>
