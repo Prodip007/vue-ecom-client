4780d11b392f1c86e342060b06aa2615ce16da8e
Prodip <p.k.sorkar007@gmail.com>
2022-09-24 15:07:26 +0600

    Category views are added and product component created

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,16 +11,8 @@
         <h2 class="is-size-2 has-text-centered">Latest products</h2>
       </div>
       <div class="columns">
-        <div class="column is-3" v-for="product in latestProducts" :key="product.id">
-          <div class="box">
-            <figure class="image md-4">
-              <img :src="product.get_thumbnail" alt="" />
-            </figure>
-            <h3 class="is-size-4">{{ product.name }}</h3>
-            <p class="is-size-6 has-text-grey">${{ product.price }}</p>
-            <router-link :to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
-          </div>
-        </div>
+        <product-box v-for="product in latestProducts" :key="product.id" :product="product" />
+
       </div>
     </div>
   </div>
@@ -30,6 +22,7 @@
 // @ is an alias to /src
 
 import axios from "axios";
+import ProductBox from '@/components/ProductBox.vue'
 
 export default {
   name: "Home",
@@ -40,14 +33,18 @@ export default {
     };
   },
 
-  components: {},
+  components: {
+    ProductBox
+  },
 
   mounted() {
     this.getLatestProducts();
+    document.title = 'Home | VEcom'
   },
   methods: {
-    getLatestProducts() {
-      axios
+    async getLatestProducts() {
+      this.$store.commit('setIsLoading', true)
+      await axios
         .get("/api/v1/latest-products/")
         .then((response) => {
           this.latestProducts = response.data;
@@ -55,15 +52,10 @@ export default {
         .catch((error) => {
           console.log(error);
         });
+      this.$store.commit('setIsLoading', false)
     },
   },
 };
 </script>
 
-<style>
-.image {
-  margin-top: -1.25rem;
-  margin-left: -1.25rem;
-  margin-right: -1.25rem;
-}
-</style>
+
